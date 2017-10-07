2b2d451a9f8dd6beab5a0e5bc8807fd7448593eb
unknown <pabliciotjg@gmail.com>
2017-10-06 22:55:24 -0300

    Primeiro envio do projeto de computaçao grafica, insercao do temple separado e estruturado com php procedural

---
+++ template/content.php
@@ -0,0 +1,16 @@
+<!-- Page Content -->
+<div id="page-wrapper">
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Page Title</h1>
+            </div>
+        </div>
+
+        <!-- ... Your content goes here ... -->
+
+    </div>
+</div>
+
+
