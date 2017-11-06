32c8b43b448ed41ebd3110904dcdc61446a8bc0e
pabliciotjg <pabliciotjg@gmail.com>
2017-11-05 23:51:23 -0200

    Base para demais operaçoes e conclusao de operacao de escala

--- canvas/js/todoenuno.js
+++ canvas/js/todoenuno.js
@@ -1,29 +1,29 @@
-	var canvas;
-	var context;
-	var width;
-	var height;
-	var data;
-	var points=[];
-	var mousemanager;
+var canvas;
+var context;
+var width;
+var height;
+var data;
+var points = [];
+var mousemanager;
 
-	function Point(xr,yr){
-	this.x=xr;
-	this.y=yr;
-	}
+function Point(xr, yr) {
+    this.x = xr;
+    this.y = yr;
+}
 
- 
-	$(document).ready(function(){
-		canvas=document.getElementById("canvas");
-		context=canvas.getContext("2d");
-		width=parseInt(canvas.getAttribute("width"));
-		height=parseInt(canvas.getAttribute("height"));
-		initializecanvas(context);
-		mousemanager = new MouseManager();
-		mousemanager.setcanvas(canvas);
-		mousemanager.setdrawmanager();
-		mousemanager.setlisteners();
-		$(document).on('change','#figura',function(){
-			mousemanager.setshape($(this).val())
-		});
-	});
+
+$(document).ready(function () {
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+    width = parseInt(canvas.getAttribute("width"));
+    height = parseInt(canvas.getAttribute("height"));
+    initializecanvas(context);
+    mousemanager = new MouseManager();
+    mousemanager.setcanvas(canvas);
+    mousemanager.setdrawmanager();
+    mousemanager.setlisteners();
+    $(document).on('change', '#figura', function () {
+        mousemanager.setshape($(this).val())
+    });
+});
 
